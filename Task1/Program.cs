3791e23609914507f0902d7ef97b67498bfe48df
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 02:37:06 +0300

    Gr8

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число, отличное от первого: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else
+{
+    Console.WriteLine($"max = {num2}");
+}
+if (num1==num2)
+{
+    Console.WriteLine("Ля, два отличных друг от друга числа!");
+}
